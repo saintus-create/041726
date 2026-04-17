e975bf72da11fa0ab2d66dd9eecc573c11916e99
saintus-create <bradleydip@gmail.com>
2026-04-17 15:46:51 -0700

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from 'next';
 import './global.css';
 import { appName } from '@/lib/shared';
 import { RootProvider } from 'fumadocs-ui/provider/next';
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: appName,
@@ -10,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body><RootProvider>{children}</RootProvider></body>
     </html>
   );
